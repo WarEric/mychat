b3ff2d021b21f37d35c4b297911aac72e3ca6508
WarEric <wareric@163.com>
2018-09-05 12:46:38 +0000

    add unix udp socket init function.

--- server.h
+++ server.h
@@ -1,8 +1,25 @@
-#ifndef SERVER_H_
-#define SERVER_H_
+/**
+ * This a server functions library
+ * by wareric@163.com
+ * 2018-09-05
+ * */
+#ifndef SERVER_H_H
+#define SERVER_H_H
 
-#define SERV_PORT 63353
-#define MAXLINE 4096
-#define LISTENQ 10
+#include<iostream>
+#include<string>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/un.h>
 
+using std::cout;
+using std::endl;
+using std::string;
+
+/*
+ * return < 0, error
+ * return >= 0, success
+ *  */
+int init_unix_bind(char *path);
 #endif
