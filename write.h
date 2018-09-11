082569daabac2d528ad85498ded8b03cc1a19db5
WarEric <wareric@163.com>
2018-09-11 06:44:42 +0000

    delete old write and read file, use wrapio replace them.

--- write.h
+++
@@ -1,9 +0,0 @@
-#ifndef WRITE_H_
-#define WRITE_H_
-#include<string>
-using std::string;
-
-ssize_t writen(int fd, const void *vptr, size_t n);
-void Writen(int fd, void *ptr, size_t nbytes, const string &type);
-
-#endif
